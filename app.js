1dfaa980f1dba42db7765f1893c214c8074b4fba
Rob <robburson07@gmail.com>
2021-11-11 16:00:03 -0600

    modularized and HTML generation

--- app.js
+++ app.js
@@ -1,31 +1,13 @@
-// console.log('Hello Node!');
+const fs = require('fs');
 
-// var message = 'Hello Node!';
+const generatePage = require('./src/page-template.js');
 
-// var sum = 5 + 3;
+const profileDataArgs = process.argv.slice(2);
 
-// console.log(message);
-// console.log(sum);
+const [name, github] = profileDataArgs;
 
-// var commandLineArgs = process.argv;
-// console.log(commandLineArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
 
-// var profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
-
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-
-
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-    }
-    console.log('================');
-
-    // Is the same as this... 
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+    console.log('Porfolio Complete! Check out index.html to see the output!');
+});
